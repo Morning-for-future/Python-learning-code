610bdd6467e33ce61a6f474b0f50635e309816ff
Slime <160466744+Morning-for-future@users.noreply.github.com>
2025-02-22 00:37:06 +0800

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,10 @@
+import numpy as np
+if True:
+    print("True")
+else:
+    print("False")
+total = item_one +\
+        item_two +\
+        item_three
+total = ['item_one','item_two',
+         'item_three']
---
+++ test4.py
@@ -0,0 +1,12 @@
+str='123456789'
+
+print(str)
+print(str[0:-3])
+print(str[0])
+print(str[1:5])
+print(str[2:])
+print(str[1:8:3])
+print(str*2)
+print(str+' no more')
+print('Hello\nrunoob')
+print(r'Hello\nrunoob')
