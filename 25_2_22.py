e5b3b5ec588865f369a716537200a2a52f4397b6
Slime <160466744+Morning-for-future@users.noreply.github.com>
2025-02-22 21:42:09 +0800

    Add files via upload

---
+++ 25_2_22.py
@@ -0,0 +1,122 @@
+# input("\n\nPress any key to exit.")
+
+# import sys; x = 'runoob' ; sys.stdout.write(x+'\n')
+# import time
+#
+# print('--Loading Runoob example : Loading effection--')
+#
+# print('Loading',end='')
+# for i in range(20):
+#     print('.',end='',flush=True)
+#     time.sleep(0.5)
+
+# import time
+# import sys
+#
+# sys.stdout.write('--Loading Runoob example : Loading effection--\n')
+# sys.stdout.write('Loading')
+# for i in range(20):
+#     sys.stdout.write('.')
+#     sys.stdout.flush()
+#     time.sleep(0.5)
+
+# import time
+# import sys
+#
+# sys.stderr.write('Loading')
+# for i in range(20):
+#     sys.stderr.write('.')
+#     sys.stderr.flush()
+#     time.sleep(0.5)
+
+# x = 'a'
+# y = 'b'
+# print(x)
+# print(y)
+# print(x,end='')
+# print(y,end='')
+
+# import math
+#
+# result = math.sqrt(16)
+# print(result)
+
+# from math import sqrt
+# result = sqrt(16)
+# print(result)
+
+# from math import sqrt as operator
+# result = operator(16)
+# print(result)
+from sys import stdout as out
+# counter = 100
+# counter1 = 1000.000
+# string = "nice day"
+# print(counter)
+
+# a=b=c=10
+# print(a,b,c)
+
+# a,b,c = 10,100.0,'nice try'
+# print(a,b,c)
+
+# a,b,c,d = 10,10.00,'good job',4+5j
+# print(type(a),type(b),type(c),type(d))
+
+# a = 111
+# print(isinstance(a,int))
+# print(isinstance(a,str))
+
+# print(1 is True)
+# print(True==1)
+# print(True+1)
+
+# print(1/2)
+# print(1//2)
+
+# print(complex(2,5)+2)
+# print(complex(2,5)*2)
+# print(complex(2,5)+2j)
+
+# str = "Hello, World!"
+# print(str)
+# print(str[0])
+# print(str[2:5])
+# print(str[2:])
+# print(str*2)
+# print(str+"TEST")
+# print(str[7:9]+str[12])
+# print(str[7:-4])
+
+# string = "Good talent"
+# print(string.upper())
+# print(string.lower())
+# print(string.capitalize())
+# print(string.title())
+# print(string.swapcase())
+# print(string.replace("talent","taste"))
+# print(string.split())
+# print(string.split("t"))
+# print(string.startswith("Good"))
+# print(string.endswith("talent"))
+# print(string.find("talent"))
+# print(string.count("t"))
+
+# word = "good mark"
+# print(word[1],word[6])#om
+# print(word[-6],word[-3])#om
+# print(word[1:-3])
+
+# letters = ['a', 'b', 'c', 'd', 'e']
+# print(letters[1:4:2])
+
+# def reverse(inputs):
+#     inputwords = input.split()
+#     inputwords = inputwords[-1::-1]
+#     output = ' '.join(inputwords)
+#     return output
+#
+# if __name__ == '__main__':
+#     input = "nice google play"
+#     reverwoerd = reverse(input)
+#     print(reverwoerd)
